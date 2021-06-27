291922df6fc53164c61f541805c876210460b9f9
b729 <mowrey729@gmail.com>
2021-06-27 09:59:41 -0500

    App rendering with some errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,242 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header class="header">
+        <img src=".../public/images/mowrey.png" alt="Bruce Mowrey" class="mowrey" />
+        <h1>Bruce Mowrey</h1>
+        
+        <nav>
+            <ul>
+                <li>
+                    <a href="#Employment-History">Employment History</a>
+                </li>
+                <li>
+                    <a href="#IT-accomplishments">IT accomplishments</a>
+                </li>
+                <li>
+                    <a href="#Full-Stack-Accomplishments">Full-Stack Accomplishments</a>
+                </li>
+                <li>
+                    <a href="#footer">Contact Information:Resume</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div className="image">
+
+        <img src="../public/images/FullStack.PNG" alt="Full Stack graphic" class="image"/>
+
+    </div>
+
+    
+     className="content"
+        <div id="Employment-History" className="Employment-History">
+            <h2>Employment History</h2>
+            <img src="./assets/images/umcems.PNG" alt="UMC EMS Logo" class="float-left" />
+
+            
+            <ul>
+                <li>
+                    * Employed by UMC EMS from June 1984 until August 2016.
+                </li>
+                <br/>
+                <li>
+                    * Field Paramedic from June 1884 until January 1993.
+                </li>
+                <br/>
+                <li>
+                    * EMS Shift Chief from January 1993 until September 2009.
+                </li>
+                <br/>
+                <li>
+                    * EMS Division Chief from September 2009 until August 2016.
+                </li>
+                <br/>
+                <li>
+                    * As EMS Division Chief responsible for IT, QI, and Training Department.
+                </li>
+            </ul>
+
+        </div>
+        <div id="IT-accomplishments" className="IT-accomplishments">
+            <h2>IT accomplishments</h2>
+            <img src="./assets/images/IMG-0177.JPG" alt="Microsoft Access Certification" class="float-left" />
+           
+            <ul>
+
+                <li>
+                    * A+ Certified, Microsoft Access 2013 Specialist Certified..
+                </li>
+                <br/>
+                <li>
+                    * 20+ years experience using Microsoft Access.
+                </li>
+                <br/>
+                <li>
+                    * 7 years experience using Crystal Reports.
+                </li>
+                <br/>
+                <li>
+                    * Developed patient care reporting application utilized by over 30 EMS agencies for 10 years..
+                </li>
+                <br/>
+                <li>
+                    *
+                    In year 2000 recognized by Texas Department of Health EMS Division as an approved EMS software
+                    vender.
+                </li>
+            </ul>
+        </div>
+        
+        
+        <div className ="container-fluid Full-Stack-Accomplishments" id="Full-Stack-Accomplishments">
+            <h2>Full-Stack Accomplishments</h2>
+            <div class="row">
+                <div class="col-3 thumb">
+                    <h4 class="float-center">Movie Review</h4>
+                    <img src="./assets/images/movieRev.PNG" alt="Movie screen shot" class="float-left fs-img" height="100px" width="150px" />
+                    <br/>
+                    <ul class="float-left">
+        
+                        <li>
+                            <a href="https://davidboerm.github.io/project-1/" target="_blank">https://davidboerm.github.io/project-1/</a>
+                        </li>
+                        <br/>
+                        <li>
+                            <a href="https://github.com/davidboerm/project-1" target="_blank">https://github.com/davidboerm/project-1</a>
+                        </li>
+        
+                    </ul>
+                </div>
+                <div className="col-3 thumb">
+                    <h4 class="float-center">Work Day Scheduler</h4>
+                    <img src="./assets/images/workday.PNG" alt="Day Sched Calendar" class="float-left fs-img" height="100px" width="150px" />
+                    <ul class="float-left">
+        
+                        <li>
+                            <a href="https://bmowrey729.github.io/Work-Day-Scheduler/" target="_blank">https://bmowrey729.github.io/Work-Day-Scheduler/</a>
+                        </li>
+                        <br/>
+                        <li>
+                            <a href="https://github.com/bmowrey729/Work-Day-Scheduler" target="_blank">https://github.com/bmowrey729/Work-Day-Scheduler</a>
+                        </li>
+        
+                    </ul>
+                </div>
+                <div className="col-3 thumb">
+                    <h4 className="float-center">Multiple Choice Quiz</h4>
+                    <img src="./assets/images/mult.PNG" alt="Workgroup" className="float-left fs-img" height="100px" width="150px" />
+                    <ul className="float-left">
+        
+                        <li>
+                            <a href="https://github.com/bmowrey729/Multiple-Choice-Quiz" target="_blank">https://github.com/bmowrey729/Multiple-Choice-Quiz</a>
+                        </li>
+                        <br/>
+                        <li>
+                            <a href="https://bmowrey729.github.io/Multiple-Choice-Quiz/" target="_blank">https://bmowrey729.github.io/Multiple-Choice-Quiz/</a>
+                        </li>
+        
+                    </ul>
+                </div>
+                <div className="col-3 thumb">
+                    <h4 className="float-center">Password Generator</h4>
+                    <img src="./assets/images/screenshot.png" alt="Workgroup" class="float-left fs-img" height="100px" width="150px" />
+                    <ul class="float-left">
+        
+                        <li>
+                            <a href="https://github.com/bmowrey729/Password-Generator" target="_blank">https://github.com/bmowrey729/Password-Generator</a>
+                        </li>
+                        <br/>
+                        <li>
+                            <a href="https://bmowrey729.github.io/Password-Generator/" target="_blank">https://bmowrey729.github.io/Password-Generator/</a>
+                        </li>
+        
+                    </ul>
+                </div>
+                </div>
+            </div>
+            <div className ="container-fluid Full-Stack-Accomplishments" id="Full-Stack-Accomplishments">
+                <h2>Full-Stack Accomplishments Continued</h2>
+                <div className="row">
+                    
+                    <div className="col-3 thumb">
+                        <h4 class="float-center">Readme Generator</h4>
+                        <img src="./assets/images/readgen.PNG" alt="Day Sched Calendar" className="float-left fs-img" height="100px" width="150px" />
+                        <ul class="float-left">
+            
+                            <li>
+                                <a href="https://github.com/bmowrey729/Readme-Generator" target="_blank">https://github.com/bmowrey729/Readme-Generator</a>
+                            </li>
+                            <br/>
+                            <li>
+                                <p>Link to video in Readme File on github</p>
+                            </li>
+            
+                        </ul>
+                    </div>
+                    
+                    <div className="col-3 thumb">
+                        <h4 class="float-center">E_Commerce_Backend</h4>
+                        <img src="./assets/images/insomniaDash.PNG" alt="Movie screen shot" className="float-left fs-img" height="100px" width="150px" />
+                        <br/>
+                        <ul className="float-left">
+            
+                            <li>
+                                <a href="https://github.com/bmowrey729/E_Commerce_BackEnd/" target="_blank">https://github.com/bmowrey729/E_Commerce_BackEnd/</a>
+                            </li>
+                            <br/>
+                            <li>
+                                <a>Link to video in Readme File on github</a>
+                            </li>
+            
+                        </ul>
+                    </div>
+                    <div class="col-3 thumb">
+                        <h4 class="float-center">Employee Tracker</h4>
+                        <img src="./assets/images/empTrack.PNG" alt="Employee Tracker screen shot" class="float-left fs-img" height="100px" width="150px" />
+                        <br/>
+                        <ul className="float-left">
+            
+                            <li>
+                                <a href="https://github.com/bmowrey729/Employee-Tracker/" target="_blank">https://github.com/bmowrey729/Employee-Tracker/</a>
+                            </li>
+                            <br/>
+                            <li>
+                                <a>Link to video in Readme File on github</a>
+                            </li>
+            
+                        </ul>
+                    </div>
+                    
+                </div>
+            
+        </div>
+    
+
+    
+    <footer>
+        <div id="footer" class="footer">
+            <h3>Contact Iinformation</h3>
+            <ul>
+                <li><strong>Phone:</strong> 806-436-5632</li>
+                <li><strong>Email:</strong> <a href="mailto:mowrey729@gmail.com">mowrey729@gmail.com</a></li>
+                <li><strong>Github Profile:</strong> <a href="https://github.com/bmowrey729" target="_blank">Bruce
+                    Mowrey</a></li>
+                <li><strong>linkedin:</strong> <a href="https://www.linkedin.com/in/bruce-mowrey-1a636651/" target="_blank">Bruce
+                        Mowrey</a></li>
+                <li><strong>Resume:</strong> <a href="./assets/docs/Bmowrey.pdf">Bmowrey.pdf</a></li>
+
+
+            </ul>
+        </div>
+       
+    </footer>
     </div>
   );
-}
+};
+
 
 export default App;
